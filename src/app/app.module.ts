da8e640b570000a0860dafc4f4041c3c90e87bff
Tim Nilsson <tim.r.nilsson@gmail.com>
2023-06-08 12:58:33 +0200

    Adds basic structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
+import { ArticleListComponent } from './article-list/article-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeaderComponent,
+    ArticleListComponent
   ],
   imports: [
     BrowserModule
