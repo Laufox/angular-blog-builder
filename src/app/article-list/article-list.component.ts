51a212d0c6383fe0c278f033d6f12a4daf9b5632
Tim Nilsson <tim.r.nilsson@gmail.com>
2023-06-08 15:05:37 +0200

    Commenting

--- src/app/article-list/article-list.component.ts
+++ src/app/article-list/article-list.component.ts
@@ -11,31 +11,39 @@ export class ArticleListComponent {
   articles: Article[] = []
   articleFormOpen: boolean = false
 
+  // Stuff thats happen on component startup
   ngOnInit(): void {
+    // Try to get values from localstorgae
     const storageArticles = localStorage.getItem('articles')
 
+    // If no values found from localstorage, return early
     if (!storageArticles) {
       return
     }
     
+    // Turn storage value into a proper array and assign it to articles state
     const parsedArticles = JSON.parse(storageArticles)
     this.articles = parsedArticles
   }
 
+  // Push new article to array and update localstorage
   addArticle(article: Article): void {
     this.articles.push(article)
     localStorage.setItem("articles",JSON.stringify(this.articles))
   }
 
+  // Delete a specific article from array and update localstorage
   removeArticle(index: number): void {
     this.articles.splice(index, 1)
     localStorage.setItem("articles", JSON.stringify(this.articles))
   }
 
+  // Open modal for adding new article
   openArticleForm(): void {
     this.articleFormOpen = true
   }
 
+  // Close modal for adding new article
   closeArticleForm(): void {
     this.articleFormOpen = false
   }
--- src/app/article.ts
+++ src/app/article.ts
@@ -1,3 +1,4 @@
+// Blueprint template for article content
 export interface Article {
     title: string,
     date: string,
