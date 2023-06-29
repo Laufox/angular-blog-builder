5e3f9dbea63471ea6fe6c6eec2d9e60bdc0d9b69
Tim Nilsson <tim.r.nilsson@gmail.com>
2023-06-29 10:56:28 +0200

    Styles header and banner

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
+// @ts-nocheck
 import { Component, Input } from '@angular/core';
+import { SiteMetaDataService } from '../site-meta-data.service';
 
 @Component({
   selector: 'app-header',
@@ -8,4 +10,14 @@ import { Component, Input } from '@angular/core';
 export class HeaderComponent {
   @Input() siteTitle = ''
   @Input() bannerImage: string | ArrayBuffer | null = null
+
+  constructor(private metaDataService: SiteMetaDataService) {}
+
+  ngOnInit() {
+    this.metaDataService.metaDataSubject.subscribe(metaData => {
+      const element = document.querySelector('.header-container')
+      element.style.backgroundImage = `url(${metaData.bannerImage ?? '../../assets/image-placeholder.png'})`
+    })
+    this.metaDataService.getMetaData()
+  }
 }
